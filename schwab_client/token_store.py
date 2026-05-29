36379e95a25a4d72c91eb32bab14a456839b94e7
Ultra Super Sam <ultrasupersam@Ultras-Mac-mini.local>
2026-05-29 16:16:56 -0700

    Add JSON token store

--- schwab_client/token_store.py
+++ schwab_client/token_store.py
@@ -4,17 +4,26 @@ from pathlib import Path
 
 class JsonTokenStore:
 
-    def __init__(self, token_file):
+    def __init__(
+        self,
+        token_file,
+    ):
         self.token_file = Path(token_file)
 
+    def exists(self):
+        return self.token_file.exists()
+
     def load(self):
-        if not self.token_file.exists():
+        if not self.exists():
             return None
 
         with open(self.token_file, "r") as file:
             return json.load(file)
 
-    def save(self, token_data):
+    def save(
+        self,
+        token_data,
+    ):
         self.token_file.parent.mkdir(
             parents=True,
             exist_ok=True,
@@ -26,3 +35,19 @@ class JsonTokenStore:
                 file,
                 indent=2,
             )
+
+    def get_access_token(self):
+        data = self.load()
+
+        if not data:
+            return None
+
+        return data.get("access_token")
+
+    def get_refresh_token(self):
+        data = self.load()
+
+        if not data:
+            return None
+
+        return data.get("refresh_token")
